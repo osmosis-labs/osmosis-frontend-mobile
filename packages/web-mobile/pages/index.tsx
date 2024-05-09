235ebc727c1169d0a0cde323947bd3548c30659e
Davide Segullo <davidesegullo@gmail.com>
2024-05-09 15:05:01 +0200

    feat: :sparkles: add web-mobile package

---
+++ packages/web-mobile/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Mobile app frontend</title>
+        <meta name="description" content="Osmosis mobile app frontend" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <p>Mobile app frontend</p>
+        </div>
+      </main>
+    </>
+  );
+}
